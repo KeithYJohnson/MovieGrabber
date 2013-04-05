198ebe04f7399fe48bbad097b6f929d1c57c9703
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-04-04 20:24:19 -0400

    adding director, plot, movie poster

--- movie_server.rb
+++ movie_server.rb
@@ -9,22 +9,11 @@ require_relative 'movies'
 enable :sessions
 use Rack::Flash	
 
-# before '/' do
-
-# 	unless params[:password] == "coolbananas"
-# 		flash[:notice] = "Sorry, the password is wrong."
-# 		redirect '/login'
-# 	end
-# end
 
 get '/' do
-
-	erb :login
-	
+	erb :login	
 end
-	# unless params[:password] == "coolbananas"
-	# 	redirect '/login'
-	# ene
+
 	
 
 get "/:name" do
@@ -47,13 +36,13 @@ end
 
 post '/film' do
 
-  newmovie = Movie.get_film_info(params[:name])
+  @newmovie = Movie.get_film_info(params[:name].gsub(" ","%20"))
   
-  @actors = newmovie.actors
+
 
   erb :movie, :locals => {
-  	:actors => @actors
+  	:newmovie => @newmovie
   }
-
+  
 
 end
